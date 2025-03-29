a92e15e24490b127db9c12f1d363cbc4c7f08d04
ohjae-88 <nokarre@gmail.com>
2025-03-29 16:16:51 +0900

    Resolved merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
+    port: 5173 // 서버 포트 설정
+  },
   build: {
     outDir: 'build', // 빌드된 파일이 이 폴더에 저장됩니다.
->>>>>>> df15f370754e23e4270f9f601ff5b7e86d6cdd69
   }
 });
