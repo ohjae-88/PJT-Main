2546647e37b0a837860843f3ee70c06485f9e84a
ohjae-88 <136108958+ohjae-88@users.noreply.github.com>
2025-03-29 15:44:23 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-<<<<<<< HEAD
-    port: 9000
-=======
-    port: 5173
->>>>>>> 9b55ddd290bd78477cf66b43ddc2f66209a12bb7
+  build: {
+    outDir: 'build', // 빌드된 파일이 이 폴더에 저장됩니다.
   }
 });
